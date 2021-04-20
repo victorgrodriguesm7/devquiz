29202b7dc7dd92a50932938479cd698e65de70b0
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-20 11:54:51 -0300

    SplashScreen Criada

---
+++ lib/screens/splash/splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear
+        ),
+        alignment: Alignment.center,
+        child: Image.asset(AppImages.logo)
+      ),
+    );
+  }
+}
